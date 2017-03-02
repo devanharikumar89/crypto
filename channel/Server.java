bf16acedf2305863943a45adff651cf9278af257
Devan Harikumar <devanharikumar89@gmail.com>
2017-03-02 10:20:11 -0500

    Committer and Verifier from different machines

--- channel/Server.java
+++ channel/Server.java
@@ -7,7 +7,7 @@ import edu.biu.scapi.comm.twoPartyComm.TwoPartyCommunicationSetup;
 import edu.biu.scapi.comm.Channel;
 import java.util.*;
 
-class SetupPartiesCopy{
+class Server{
 public static void main(String[]args){
 LoadSocketParties loadParties = new LoadSocketParties("socket.properties");
 List<PartyData> listOfParties = loadParties.getPartiesList();
@@ -15,13 +15,16 @@ for(PartyData data : listOfParties){
 	System.out.println(data.toString());
 }
 try{
-TwoPartyCommunicationSetup commSetup = new SocketCommunicationSetup(listOfParties.get(1), listOfParties.get(0));
+TwoPartyCommunicationSetup commSetup = new SocketCommunicationSetup(listOfParties.get(0), listOfParties.get(1));
 
 //Call the prepareForCommunication function to establish one connection within 2000000 milliseconds.
 Map<String, Channel> connections = commSetup.prepareForCommunication(1, 2000000);
+
+//Return the channel to the calling application. There is only one created channel.
 Channel channel = (Channel) connections.values().toArray()[0];
-String data = (String)channel.receive();
-System.out.println("Received : "+data);
+System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+channel.toString());
+System.out.println("Sending data");
+channel.send("devan");
 
 }catch(Exception e){
 System.out.println("Exception");
