0cd3f0e1a9722b95c25e087c3f9c4b6bda1743fe
Devan Harikumar <devanharikumar89@gmail.com>
2017-03-30 16:27:10 -0400

    RS changes

--- Reed Solomon Codes/RS.java
+++ Reed Solomon Codes/RS.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.BigInteger;
 import edu.biu.scapi.primitives.dlog.DlogGroup;
 import edu.biu.scapi.primitives.dlog.miracl.MiraclDlogECF2m;
 import edu.biu.scapi.primitives.dlog.GroupElement;
@@ -8,36 +9,51 @@ import edu.biu.scapi.primitives.dlog.ZpElement;
 
 
 class RS{
-	public static void main(String[]args) throws Exception{
-		String input = args[0];
-		int t=Integer.parseInt(args[1]);
-		//This means we have to divide the input string into length/(t+1) groups
-		List<String>parts = new ArrayList<String>();
-		int pad = (t+1)-(input.length() % (t+1));
-		input += String.format("%1$-"+(pad)+"s", "" ).replace(" ", "0");
-		System.out.println(input);
+	String input;
+	int t;//Number of parts
+	int N;//
+	List<String>parts;
+	List<GroupElement>partsAsElements;
+	List<BigInteger>coefficients;
+	OpenSSLDlogZpSafePrime dlog;
 
-		int partLength = input.length()/(t+1);
+	public RS(){
+		this.parts = new ArrayList<String>();
+		this.partsAsElements = new ArrayList<GroupElement>();
+		this.dlog = new OpenSSLDlogZpSafePrime();
+		coefficients = new ArrayList<BigInteger>();
+	}
+
+	public static void main(String[]args) throws Exception{
+		RS rs = new RS();
+		rs.input = args[0];
+		rs.t=Integer.parseInt(args[1]);
+		//This means we have to divide the input string into length/(t+1) groups
+		int pad = (rs.t+1)-(rs.input.length() % (rs.t+1));
+		rs.input += String.format("%1$-"+(pad)+"s", "" ).replace(" ", "0");
+		System.out.println(rs.input);
+
+		int partLength = rs.input.length()/(rs.t+1);
 		int i=0;
-		while(i<input.length() && i+partLength<input.length()){
-			parts.add(input.substring(i, i+partLength));
+		while(i<rs.input.length() && i+partLength<rs.input.length()){
+			rs.parts.add(rs.input.substring(i, i+partLength));
 			i+=partLength;
 		}
-		if(i<input.length()){
-			parts.add(input.substring(i));
+		if(i<rs.input.length()){
+			rs.parts.add(rs.input.substring(i));
 		}
-		System.out.println(parts.toString());
+		System.out.println(rs.parts.toString());
 
 		//At this point the input string is split into parts
 		//Now we need to convert each part into a field element.
 
-		List<GroupElement>partsAsElements = new ArrayList<GroupElement>();
-		OpenSSLDlogZpSafePrime dlog = new OpenSSLDlogZpSafePrime();
-		for(String part : parts){
+		
+		
+		for(String part : rs.parts){
 			//System.out.println(new String(part.getBytes()));
-			GroupElement ge = dlog.encodeByteArrayToGroupElement(part.getBytes());
+			GroupElement ge = rs.dlog.encodeByteArrayToGroupElement(part.getBytes());
 			//System.out.println(ge.toString());
-			partsAsElements.add(ge);
+			rs.partsAsElements.add(ge);
 		}
 		//System.out.println(partsAsElements.toString());
 		//Done
@@ -45,16 +61,34 @@ class RS{
 		//Now we need to convert each part into a field element.
 		//p(x) = m0 + m1.x + m2.x^2 + m3.x^3 + ... + mt.x^t;
 		int j=0;
-		for(GroupElement ge : partsAsElements){
+		for(GroupElement ge : rs.partsAsElements){
 			//dlog.decodeGroupElementToByteArray((ECF2mPointBc)ge)
-			System.out.println("m"+j+" : "+((ZpElement)ge).getElementValue());
+			BigInteger coefficient = ((ZpElement)ge).getElementValue();
+			System.out.println("m"+j+" : "+coefficient);
+			rs.coefficients.add(coefficient);
 			j++;
 			//System.out.println(new String(dlog.decodeGroupElementToByteArray(ge)));
 		}
-		
+		//System.out.println(rs.coefficients);
 		
 
+		//Defining the polynomial part is over at this point.
+		//Next we start computing the code word.
+		//We compute p(x) for n different x values.
+		rs.evaluatePolynomial(new BigInteger("23423"));
 
+	}
+	public BigInteger evaluatePolynomial(BigInteger alpha){
+		int power = 0;
+		BigInteger sum = BigInteger.ZERO;
+
+		for(int k=0; k<=t; k++){
+			BigInteger term = coefficients.get(k).multiply(alpha.pow(k));
+			sum=sum.add(term);
+		}
+
+		System.out.println(sum);
+		return sum;
 
 	}
 }
